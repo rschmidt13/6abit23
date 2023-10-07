7a68d400a6b94079e76a616b62504b3c9bba7819
LukasQL <128224151+Lukas-Poll@users.noreply.github.com>
2023-10-07 15:37:41 +0200

    von Desiree Thomas Lukas

--- DDL.sql
+++ DDL.sql
@@ -1,4 +1,14 @@
 -- Projekt Klassenverwaltung
 -- DDL Anweisungen
 
+drop schema if exists Schule_2023 cascade;
+create schema if not exists Schule_2023;
+set search_path to Schule_2023;
 
+drop table if exists Schueler cascade;
+create table Schueler(
+	MNr serial primary key,
+	Vorname varchar(25),
+	Nachname varchar(25),
+	Geb_Datum date
+);
