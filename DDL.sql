ca665f2d6ab36a8fec2e02b6ff64c43944408033
Rainer <rainer@sciserver.at>
2023-12-12 20:32:13 +0100

    DDL tabelle schueler

--- DDL.sql
+++ DDL.sql
@@ -1,4 +1,15 @@
 -- Projekt Klassenverwaltung
 -- DDL Anweisungen
 
+drop table if exists SCHUELER cascade;
+
+CREATE TABLE SCHUELER (
+	sid int primary key,
+	vorname varchar(255),
+	nachname varchar(255),
+	gebdatum date
+);
+
+insert into SCHUELER values (1, 'Susi', 'Moser');
+
 
