4a26a5d1c1d0eb65565fd3d7f2583519d0aa7464
Rainer <rainer@sciserver.at>
2023-12-12 21:05:54 +0100

    added sequence

--- DDL.sql
+++ DDL.sql
@@ -11,5 +11,26 @@ CREATE TABLE SCHUELER (
 );
 
 insert into SCHUELER values (1, 'Susi', 'Moser');
+insert into SCHUELER values (10, 'Susi2', 'Moser');
+insert into SCHUELER values (20, 'Susi3', 'Moser');
+
+select * from schueler s ;
+
+create sequence seq_schueler_sid
+start 10
+increment 10
+--owned by schueler.sid
+;
+
+drop sequence seq_schueler_sid;
+
+insert into SCHUELER values (nextval('seq_schueler_sid'), 'Max', 'Maier');
+
+select * from schueler;
+
+alter table schueler alter column sid set default nextval('seq_schueler_sid');  
+
+insert into SCHUELER (vorname, nachname) values ('Hans', 'Huber');
+
 
 
